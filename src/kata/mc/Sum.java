077bfa0a9057115c0b7f44f9a4fdb28980664329
Marcin Czernek <mustang.mcz@gmail.com>
2021-06-10 18:05:19 +0200

    Initial commit

---
+++ src/kata/mc/Sum.java
@@ -0,0 +1,23 @@
+package kata.mc;
+
+public class Sum {
+
+
+    public static void main(String[] args) {
+        double[] tablica = {1.1, 2.2, 3.3};
+        sum(tablica);
+        System.out.println(sum(tablica));
+
+    }
+
+    public static double sum(double[] numbers) {
+        double suma = 0;
+        for (double i : numbers) {
+            suma += i;
+        }
+        return suma;
+    }
+
+}
+
+
